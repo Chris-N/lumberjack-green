70f8b81ebf195949d3ecc3e582aaff99168b6149
Chris-N <sdchrisn@gmail.com>
2022-05-25 17:21:35 -0700

    Created new title scene with function UI button to start game

--- Assets/Scripts/Helper/Constants.cs
+++ Assets/Scripts/Helper/Constants.cs
@@ -1,5 +1,9 @@
 public static class Constants
 {
+    // GameObjects
     public static readonly string BLADE = "Blade";
     public static readonly string WOOD = "Wood";
+
+    // Scene
+    public static readonly string FOREST = "Forest";
 }
